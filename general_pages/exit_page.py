449e8aa3afd8f580d49afb8960db4ae84a464595
Shivam Kundan <shivamkundan@hotmail.com>
2023-04-23 04:35:28 -0500

    split exit page into new file

---
+++ general_pages/exit_page.py
@@ -0,0 +1,34 @@
+import pygame
+import logging
+import time, datetime
+from page_templates import PageTemplate
+from paths_and_utils import FULL_SCREEN_RES
+from sys import exit
+
+start_time_overall=time.time()
+
+def wrap_up():
+		print ('\nLive long & prosper... bye!')
+		t=elapsed_time(print_res=True)
+
+def elapsed_time(print_res=False):
+		end_time=round(time.time()-start_time_overall,2)
+		elapsed_time=(datetime.timedelta(seconds=round(end_time)))
+		if (print_res):
+			logging.info (f'time elapsed: {elapsed_time} s')
+		return elapsed_time
+
+# Faux page for clean exit
+class ExitPage(PageTemplate):
+	def __init__(self,name):
+		super().__init__(name)
+
+	def next_frame(self,screen,curr_events,**kwargs):
+		#  Disbale fullscreen
+		if pygame.display.get_window_size()==FULL_SCREEN_RES:
+			logging.info ('disabling full screen')
+			pygame.display.toggle_fullscreen()
+
+		wrap_up()
+		pygame.quit()
+		exit()
--- tricorder.py
+++ tricorder.py
@@ -71,6 +71,7 @@ from fly_page import *
 from developer_page import *
 from home_page import *
 from sleep_page import *
+from exit_page import *
 
 # ===================== Helper Classes =============================== #
 class thread_with_trace(threading.Thread):
@@ -274,21 +275,6 @@ class SliderClass():
 		return (x - in_min) * (out_max - out_min) / (in_max - in_min) + out_min
 
 # ====================== General Pages =============================== #
-
-# Faux page for clean exit
-class ExitPage(PageTemplate):
-	def __init__(self,name):
-		super().__init__(name)
-
-	def next_frame(self,screen,curr_events,**kwargs):
-		#  Disbale fullscreen
-		if pygame.display.get_window_size()==FULL_SCREEN_RES:
-			logging.info ('disabling full screen')
-			pygame.display.toggle_fullscreen()
-
-		wrap_up()
-		pygame.quit()
-		sys.exit()
 
 class DeviceStatsPage(DeviceStatsPageTemplate):
 	def __init__(self,name):
@@ -1551,20 +1537,10 @@ class WindowManager():
 		pygame.image.save(sub,name)
 		self.screen.blit(camera,(20,160))
 # ==================================================================== #
-def wrap_up():
-		print ('\nLive long & prosper... bye!')
-		t=elapsed_time(print_res=True)
-
-def elapsed_time(print_res=False):
-		end_time=round(time.time()-start_time_overall,2)
-		elapsed_time=(datetime.timedelta(seconds=round(end_time)))
-		if (print_res):
-			logging.info (f'time elapsed: {elapsed_time} s')
-		return elapsed_time
 
 if __name__=='__main__':
 
-	global start_time_overall
+	# global start_time_overall
 	global MODE
 	global PIGPIO
 
@@ -1572,7 +1548,7 @@ if __name__=='__main__':
 	# PIGPIO=pigpio.pi()
 
 	MODE='normal'
-	start_time_overall=time.time()
+	# start_time_overall=time.time()
 
 	clock=pygame.time.Clock()
 
