b7ed869284a68402ae3fc69f19207bce59b09aa9
Shivam Kundan <shivamkundan@hotmail.com>
2023-05-25 23:02:21 -0500

    Update tricorder.py

--- tricorder.py
+++ tricorder.py
@@ -24,7 +24,6 @@
 # @package bluetooth Bluetooth-related communication code.
 # @package resources Contains custom user events, logging code, and serial comm code mappings.
 
-
 # https://stackoverflow.com/questions/36064976/using-doxygen-in-read-the-docs
 
 import pygame
